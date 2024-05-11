89ca881398f346b8d9941f5ccd189dfc97f81e05
SagarEGme <sagaregmi0710@gmail.com>
2024-05-11 18:33:40 +0545

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/",(req,res)=>{
-    res.send("route successfull.")
-})
-app.listen(8000,()=>console.log("Server running on port 8000"))
+app.get("/", (req, res) => {
+  res.send("route successfull.");
+});
+app.listen(8000, () => console.log("Server running on port 8000"));
